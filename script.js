c10a05374a6ae98199764f7afc846eb0151f1c48
MFirozAlam <146247719+MFirozAlam@users.noreply.github.com>
2026-04-03 14:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,23 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	const values = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I']
+  ];
 
-  //your code here
-	let result='';
-	 for (let i = 0; i <= 6; i++) {
-    let symbol = obj[i][0];
-    let value = obj[i][1];
+  let result = '';
 
-    // Handle 9 cases (like 900, 90, 9)
-    if (i % 2 === 0 && i + 2 <= 6) {
-      let nextValue = obj[i + 2][1];
-      if (num >= value - nextValue) {
-        while (num >= value - nextValue) {
-          result += obj[i + 2][0] + symbol;
-          num -= (value - nextValue);
-        }
-      }
-    }
-
-    // Handle 4 cases (like 400, 40, 4)
-    if (i % 2 === 0 && i + 1 <= 6) {
-      let halfValue = obj[i + 1][1];
-      if (num >= value - halfValue) {
-        while (num >= value - halfValue) {
-          result += symbol + obj[i + 1][0];
-          num -= (value - halfValue);
-        }
-      }
-    }
-
-    // Normal cases
+  for (let [value, symbol] of values) {
     while (num >= value) {
       result += symbol;
       num -= value;
@@ -45,7 +25,6 @@ function convertToRoman(num) {
   }
 
   return result;
-
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
